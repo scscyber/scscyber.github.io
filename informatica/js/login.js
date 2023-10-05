c623b127df678ecca2eed435b58e14d05f4ad825
Scscyber <64031790+scscyber@users.noreply.github.com>
2023-10-05 18:38:42 -0300

    Update login.js

--- informatica/js/login.js
+++ informatica/js/login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "admin"){
 alert ("Efetuado com sucesso !!!");
-window.location = "../../../index.html"; // Redirecting to other page.
+window.location = "https://scscyber.github.io/index.html"; // Redirecting to other page.
 return false;
 }
 else{
